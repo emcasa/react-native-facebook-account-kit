3a1a3d5c956672388113de3dee0395931a25632a
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-08-31 17:24:57 -0300

    Set react subview frame

--- ios/RNAccountKitUIView.m
+++ ios/RNAccountKitUIView.m
@@ -20,11 +20,37 @@
 
 - (CGSize)intrinsicContentSize
 {
-    return _subView.intrinsicContentSize;
+    return CGSizeMake(self.bounds.size.width, _subView.intrinsicContentSize.height);
 }
 
 - (void)rootViewDidChangeIntrinsicSize:(RCTRootView *)rootView {
     [self invalidateIntrinsicContentSize];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    _subView.frame = self.bounds;
+}
+
+- (CGSize)sizeThatFits:(CGSize)size
+{
+    return CGSizeMake(self.bounds.size.width, MAX(size.height, _subView.intrinsicContentSize.height));
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView *view = _subView.contentView;
+    if (!view.hidden && [view pointInside:[self convertPoint:point toView:view] withEvent:event]) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)dealloc
+{
+    NSLog(@"DEALLOC");
+    self.subView = nil;
 }
 
 @end
